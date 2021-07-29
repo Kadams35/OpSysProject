569cb8e9ead153cb69740c35ce5a11c765f0a77f
asandonato <72477245+asandonato@users.noreply.github.com>
2021-07-29 08:11:52 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -9,6 +8,7 @@
 #include <stdlib.h>
 //#include <cassert>
 #include "Process.h"
+#include "FCFS.h"
 
 
 double next_exp(double lambda, int upper_bound){
@@ -26,16 +26,15 @@ std::vector<Process> mainlist(int n, int seed, double lambda, int upper_bound) {
     srand48(seed); //Sets the seed (this needs to be done before each method to reset the randomization)
     std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     std::vector<Process> Main_list;
+
     for (int i = 0; i < n; i++) {
         char id = alphabet[i];
         int arrival = next_exp(lambda, upper_bound);
         int num_cpu_bursts = ceil(drand48() * 100);
 
-        std::vector<long int> cpu_bursts;
+        std::vector<int> cpu_bursts;
         std::vector<int> io_bursts;
         for (int j = 0; j < num_cpu_bursts; j++) {
-            long int temp;
-            int temp1;
             if (j == num_cpu_bursts - 1) {
                 int temp = ceil(next_exp(lambda, upper_bound));
                 int temp1 = 0;
@@ -47,14 +46,14 @@ std::vector<Process> mainlist(int n, int seed, double lambda, int upper_bound) {
                 int temp1 = ceil(next_exp(lambda, upper_bound))*10;
                 cpu_bursts.push_back(temp);
                 io_bursts.push_back(temp1);
+
             }
         }
-        Main_list.push_back(Process(id, arrival, cpu_bursts, io_bursts));
 
+        Main_list.push_back(Process(id, arrival, cpu_bursts, io_bursts));
     }
     return Main_list;
 }
-
 
 
 
@@ -74,7 +73,11 @@ int main(int argc, char* argv[]) {
 
     
     std::vector<Process> Main_list = mainlist(n, seed, lambda, upper_bound);
+
     //send to fcfsfsfscsdssfscdcd
+
+    FCFS test = FCFS(Main_list, t_cs);
+    test.FCFS_algorithm();
 
     return EXIT_SUCCESS;
 }
